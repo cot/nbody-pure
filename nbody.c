18d623e33290e329a201a50fbf790b0b3177bb1f
cot <rue@cotux>
2013-04-11 13:56:49 +0200

    modified:   nbody.c 	modified:   proto.c 	modified:   proto.h

--- nbody.c
+++ nbody.c
@@ -26,12 +26,19 @@ int main() {
 
 #ifdef SOLAIRE
 	for (i=0;i<npart;i++) {
-		m[i] = 1.8986e+27 * rand() + 1.314e+22 ; /* masse des planetes du systeme solaire: 1.314*1e22 < m < 1.8986*1e27 (en kg)*/
 		for (j=0;j<dim;j++) {
-			r[i][j] = (5914.65*1e6 * rand() + 57.9*1e6) * 1.0e+3 ; /* distance moyenne au soleil: 57.9 < d < 5914.65 (en millions de km) */
+			r[i][j] = 0.0;
 		}
 	}
-
+	m[0] = 1.9891*1e30;	
+	m[1] = 0.33*1e24;	r[1][1] = 57.8*1.0e+6;
+	m[2] = 4.87*1e24;					r[2][2] = 108.2*1.0e+6;
+	m[3] = 5.9736*1e24;	r[3][1] = -149.6*1.0e+6;	r[3][2] = 149.6*1.0e+6;
+	m[4] = 0.642*1e24;					r[4][2] = 227.9*1.0e+6;	
+	m[5] = 1899*1e24;	r[5][1] = -778.6*1.0e+6;	r[5][2] = 778.6*1.0e+6;
+	m[6] = 568*1e24;	r[6][1] = 1433.5*1.0e+6;	r[6][2] = -1433.5*1.0e+6;
+	m[7] = 86.8*1e24;					r[7][2] = 2872.5*1.0e+6;
+	m[8] = 102*1e24;	r[8][1] = 4495.1*1.0e+6;	r[8][2] = 4495.1*1.0e+6;
 #else
 	for (i=0;i<npart;i++) {
 		m[i] = 1.8986e+27 * rand() + 1.314e+22 ; /* masse des planetes du systeme solaire: 1.314*1e22 < m < 1.8986*1e27 (en kg)*/
@@ -51,7 +58,7 @@ int main() {
 	printf(" --> La vitesse estimée suivant x vaut: %g km.s^-1\n",EvalVitesseX(alea));
 	printf(" --> La vitesse estimée suivant Y vaut: %g km.s^-1\n",EvalVitesseY(alea));
 	printf(" --> La vitesse estimée suivant Z vaut: %g km.s^-1\n",EvalVitesseZ(alea));
-
+	printf(" la nouvelle position est alors: (%g,%g,%g) \n",EvolutionX(alea),EvolutionY(alea),EvolutionZ(alea));
 
 	printf("_________________________________________________________________________\n\n");
 	printf("everything's good \n");
--- proto.c
+++ proto.c
@@ -4,14 +4,40 @@
 
 #include "proto.h"
 
+
+/**
+ *\author François Rué
+ *\version 0.1
+ *\date 11 avril 2013
+ *\file proto.c
+ *\brief Probleme a n-corps multidimensionnel pour le cas d'effet gravitationnel
+ */
+
+/*! \fn double Norme(double A[3], double B[3])
+ *  \brief Permet d'evaluer la norme L2 de la différence de deux vecteurs A et B, représentés en coordonnées cartésiennes (x,y,z)
+ *  \param[in] "A[3]" premier vecteur de type struct array
+ *  \param[in] "B[3]" second vecteur de type struct array
+ *  \return toutes les fonctions de proto.c sont créées pour n'effectuer que des retours de type double
+ */
 double Norme(double A[3], double B[3]) {
 	return(sqrt( (A[0]-B[0])*(A[0]-B[0]) + (A[1]-B[1])*(A[1]-B[1]) + (A[2]-B[2])*(A[2]-B[2])  ));
 }
 
+/*! \fn double UnCube(double v)
+ *  \brief Permet d'evaluer l'inverse d'une valeur élevée au cube
+ *  \param[in] "v" valeur à évaluer au cube et à inverser (dans le cas n-body: la distance entre deux entitées)
+ *  \return toutes les fonctions de proto.c sont créées pour n'effectuer que des retours de type double
+ */
 double UnCube(double v) {
 	return(1.0/(v*v*v));
 }
 
+/*! \fn double Distance(double v, double w)
+ *  \brief Permet d'evaluer la distance entre deux points: cette fonction permet d'évaluer chaque entitée dans chaque direction
+ *  \param[in] "v" première valeur d'entrée
+ *  \param[in] "w" seconde valeur d'entrée
+ *  \return toutes les fonctions de proto.c sont créées pour n'effectuer que des retours de type double
+ */
 double Distance(double v, double w) {
 	return(v-w);
 }
